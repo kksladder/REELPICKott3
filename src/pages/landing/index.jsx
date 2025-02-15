9b5a1d8d23c8ffdf6267ef4ec6277dc0e1d80894
kisupkim <kksladder@gmail.com>
2025-02-15 16:42:54 +0900

    멤버십 고객센터 디자인중

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from 'react';
-import { OverboardWindow, Overboard, ImagesPlacer, MoviePoster } from './style';
+import { useEffect, useState } from "react";
+import { OverboardWindow, Overboard, ImagesPlacer, MoviePoster } from "./style";
 
 const Gallery = () => {
     const movies = [
-        { id: 1, title: 'Movie 1', poster: '/images/22.jpg', area: 'a' },
-        { id: 2, title: 'Movie 2', poster: '/images/23.jpg', area: 'b' },
-        { id: 3, title: 'Movie 3', poster: '/images/24.jpg', area: 'c' },
-        { id: 4, title: 'Movie 4', poster: '/images/AAA.jpg', area: 'd' },
-        { id: 5, title: 'Movie 5', poster: '/images/pandabear.jpg', area: 'e' },
-        { id: 6, title: 'Movie 6', poster: '/images/monster.jpg', area: 'f' },
-        { id: 7, title: 'Movie 7', poster: '/images/parasite.jpg', area: 'g' },
-        { id: 8, title: 'Movie 8', poster: '/images/mother.jpg', area: 'h' },
-        { id: 9, title: 'Movie 9', poster: '/images/memorykilling.jpg', area: 'i' },
-        { id: 10, title: 'Movie 10', poster: '/images/okja.jpg', area: 'j' },
-        { id: 11, title: 'Movie 11', poster: '/images/janhwa.jpg', area: 'k' },
-        { id: 12, title: 'Movie 12', poster: '/images/lifeishoney.jpg', area: 'l' },
-        { id: 13, title: 'Movie 13', poster: '/images/smallLOGO.svg', area: 'm' },
-        { id: 14, title: 'Movie 14', poster: '/images/ret.jpg', area: 'n' },
-        { id: 15, title: 'Movie 15', poster: '/images/dog.jpg', area: 'o' },
-        { id: 16, title: 'Movie 16', poster: '/images/snowpiecer.jpg', area: 'p' },
+        { id: 1, title: "Movie 1", poster: "/images/22.jpg", area: "a" },
+        { id: 2, title: "Movie 2", poster: "/images/23.jpg", area: "b" },
+        { id: 3, title: "Movie 3", poster: "/images/24.jpg", area: "c" },
+        { id: 4, title: "Movie 4", poster: "/images/AAA.jpg", area: "d" },
+        { id: 5, title: "Movie 5", poster: "/images/pandabear.jpg", area: "e" },
+        { id: 6, title: "Movie 6", poster: "/images/monster.jpg", area: "f" },
+        { id: 7, title: "Movie 7", poster: "/images/parasite.jpg", area: "g" },
+        { id: 8, title: "Movie 8", poster: "/images/mother.jpg", area: "h" },
+        { id: 9, title: "Movie 9", poster: "/images/memorykilling.jpg", area: "i" },
+        { id: 10, title: "Movie 10", poster: "/images/okja.jpg", area: "j" },
+        { id: 11, title: "Movie 11", poster: "/images/janhwa.jpg", area: "k" },
+        { id: 12, title: "Movie 12", poster: "/images/lifeishoney.jpg", area: "l" },
+        { id: 13, title: "Movie 13", poster: "/images/smallLOGO.svg", area: "m" },
+        { id: 14, title: "Movie 14", poster: "/images/ret.jpg", area: "n" },
+        { id: 15, title: "Movie 15", poster: "/images/dog.jpg", area: "o" },
+        { id: 16, title: "Movie 16", poster: "/images/snowpiecer.jpg", area: "p" },
     ];
 
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -35,8 +35,8 @@ const Gallery = () => {
             setMousePosition({ x: -xPercent, y: -yPercent });
         };
 
-        window.addEventListener('mousemove', handleMouseMove);
-        return () => window.removeEventListener('mousemove', handleMouseMove);
+        window.addEventListener("mousemove", handleMouseMove);
+        return () => window.removeEventListener("mousemove", handleMouseMove);
     }, []);
 
     return (
